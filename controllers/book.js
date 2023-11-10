bab36cda6e5eb73212f86c69a8b286754011edf5
SunnithReddyKondam <142992377+sunnithreddykondam@users.noreply.github.com>
2023-11-10 08:41:35 -0600

    updated to update the instances

--- controllers/book.js
+++ controllers/book.js
@@ -22,7 +22,15 @@ exports.book_view_all_Page = async function (req, res) {
 };
 // for a specific Book.
 exports.book_detail = async function (req, res) {
-  res.send("NOT IMPLEMENTED: Book detail: " + req.params.id);
+  // res.send("NOT IMPLEMENTED: Book detail: " + req.params.id);
+  console.log("detail" + req.params.id);
+  try {
+    result = await Book.findById(req.params.id);
+    res.send(result);
+  } catch (error) {
+    res.status(500);
+    res.send(`{"error": document for id ${req.params.id} not found`);
+  }
 };
 // Handle Book create on POST.
 exports.book_create_post = async function (req, res) {
@@ -50,5 +58,21 @@ exports.book_delete = async function (req, res) {
 };
 // Handle Book update form on PUT.
 exports.book_update_put = async function (req, res) {
-  res.send("NOT IMPLEMENTED: Book update PUT" + req.params.id);
+  // res.send("NOT IMPLEMENTED: Book update PUT" + req.params.id);
+  console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`);
+  try {
+    let toUpdate = await Book.findById(req.params.id);
+    // Do updates of properties
+    if (req.body.name) toUpdate.name = req.body.name;
+    if (req.body.cost) toUpdate.cost = req.body.cost;
+    if (req.body.size) toUpdate.size = req.body.size;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result);
+    res.send(result);
+  } catch (err) {
+    res.status(500);
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+  }
 };
--- routes/resource.js
+++ routes/resource.js
@@ -13,7 +13,7 @@ router.delete("/books/:id", book_controller.book_delete);
 // PUT request to update Book.
 router.put("/books/:id", book_controller.book_update_put);
 // GET request for one Book.
-// router.get("/books/:id", book_controller.book_detail);
+router.get("/books/:id", book_controller.book_detail);
 // GET request for list of all Book items.
 // router.get("/books", book_controller.book_list);
 router.get("/books", book_controller.book_view_all_Page);
