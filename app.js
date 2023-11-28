c28891c6bb9bdb304573a65894a3d3cab3da0532
SunnithReddyKondam <142992377+sunnithreddykondam@users.noreply.github.com>
2023-11-28 16:24:59 -0600

    added passport module created login/register forms

--- app.js
+++ app.js
@@ -4,6 +4,29 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var Book = require("./models/book");
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+
+passport.use(
+  new LocalStrategy(function (username, password, done) {
+    Account.findOne({ username: username })
+      .then(function (user) {
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false, { message: "Incorrect username." });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: "Incorrect password." });
+        }
+        return done(null, user);
+      })
+      .catch(function (err) {
+        return done(err);
+      });
+  })
+);
 
 require("dotenv").config();
 const connectionString = process.env.MONGO_CON;
@@ -77,6 +100,16 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(
+  require("express-session")({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
@@ -87,6 +120,12 @@ app.use("/choose", chooseRouter);
 app.use("/resource", resourceRouter);
 
 // catch 404 and forward to error handler
+
+var Account = require("./models/account");
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
 app.use(function (req, res, next) {
   next(createError(404));
 });
